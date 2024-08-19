085bba7fea0a45dbbc94c50e37fd117ed2ea1fea
DingJiaxiong <1612102142@qq.com>
2024-08-19 22:34:55 +0800

    netty 实现 websocket 编码

--- lzuchat-chat-server/src/main/java/com/dingjiaxiong/lzuchat/common/websocket/NettyWebSocketServerHandler.java
+++ lzuchat-chat-server/src/main/java/com/dingjiaxiong/lzuchat/common/websocket/NettyWebSocketServerHandler.java
@@ -1,8 +1,6 @@
 package com.dingjiaxiong.lzuchat.common.websocket;
 
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.extra.spring.SpringUtil;
-import cn.hutool.json.JSONUtil;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -20,38 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 @Sharable
 public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
-    private WebSocketService webSocketService;
-
-    // 当web客户端连接后，触发该方法
-    @Override
-    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        this.webSocketService = getService();
-    }
-
-    // 客户端离线
-    @Override
-    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        userOffLine(ctx);
-    }
-
-    /**
-     * 取消绑定
-     *
-     * @param ctx
-     * @throws Exception
-     */
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        // 可能出现业务判断离线后再次触发 channelInactive
-        log.warn("触发 channelInactive 掉线![{}]", ctx.channel().id());
-        userOffLine(ctx);
-    }
-
-    private void userOffLine(ChannelHandlerContext ctx) {
-        this.webSocketService.removed(ctx.channel());
-        ctx.channel().close();
-    }
-//
     /**
      * 心跳检查
      *
@@ -61,32 +27,12 @@ public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<Tex
      */
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        if (evt instanceof IdleStateEvent) {
-            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
-            // 读空闲
-            if (idleStateEvent.state() == IdleState.READER_IDLE) {
-                // 关闭用户的连接
-                userOffLine(ctx);
-            }
-        } else if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-            this.webSocketService.connect(ctx.channel());
-            String token = NettyUtil.getAttr(ctx.channel(), NettyUtil.TOKEN);
-            if (StrUtil.isNotBlank(token)) {
-                this.webSocketService.authorize(ctx.channel(), new WSAuthorize(token));
-            }
+
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+
+            // 握手完成
+            System.out.println("握手完成");
         }
-        super.userEventTriggered(ctx, evt);
-    }
-
-    // 处理异常
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        log.warn("异常发生，异常消息 ={}", cause);
-        ctx.channel().close();
-    }
-
-    private WebSocketService getService() {
-        return SpringUtil.getBean(WebSocketService.class);
     }
 
     // 读取客户端发送的请求报文
@@ -97,17 +43,6 @@ public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<Tex
 
         System.out.println(text);
 
-//        WSBaseReq wsBaseReq = JSONUtil.toBean(msg.text(), WSBaseReq.class);
-//        WSReqTypeEnum wsReqTypeEnum = WSReqTypeEnum.of(wsBaseReq.getType());
-//        switch (wsReqTypeEnum) {
-//            case LOGIN:
-//                this.webSocketService.handleLoginReq(ctx.channel());
-//                log.info("请求二维码 = " + msg.text());
-//                break;
-//            case HEARTBEAT:
-//                break;
-//            default:
-//                log.info("未知类型");
 //        }
     }
 }
